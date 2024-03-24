2c333fcebb538bcfec99a0780c703fbfd902603d
claytonikuta <clayton.ikuta@gmail.com>
2024-03-24 12:41:07 -0700

    base

---
+++ src/app/data/tags.ts
@@ -0,0 +1,11 @@
+import { Tag } from '../models/tag';
+
+export const TAGS: Tag[] = [
+  { id: 1, name: 'JavaScript', slug: 'javascript' },
+  { id: 2, name: 'C#', slug: 'c#' },
+  { id: 3, name: 'Angular', slug: 'angular' },
+  { id: 4, name: 'TypeScript', slug: 'typescript' },
+  { id: 5, name: 'React', slug: 'react' },
+  { id: 6, name: 'HTML', slug: 'html' },
+  { id: 7, name: 'CSS', slug: 'css' },
+];
